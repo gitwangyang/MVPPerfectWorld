7e4876e335b99a1c1626e30a5c76cb00b23502d5
gitwangyang <gitwangyang@aliyun.com>
2019-03-20 09:12:12 +0800

    first commit

--- app/src/main/java/com/dotawang/mvpperfectworld/ui/login/LoginActivity.java
+++ app/src/main/java/com/dotawang/mvpperfectworld/ui/login/LoginActivity.java
@@ -10,8 +10,6 @@ import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.util.Map;
-
 import com.dotawang.mvpperfectworld.R;
 import com.dotawang.mvpperfectworld.base.IView;
 import com.dotawang.mvpperfectworld.base.MVPBaseActivity;
@@ -19,58 +17,75 @@ import com.dotawang.mvpperfectworld.ui.login.contract.LoginContract;
 import com.dotawang.mvpperfectworld.ui.login.presenter.LoginPresenter;
 import com.dotawang.mvpperfectworld.utils.custom.EditTextWithDeleteIcon;
 
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * @author DotaWang
  */
-public class LoginActivity extends MVPBaseActivity<LoginPresenter> implements LoginContract.View{
+public class LoginActivity extends MVPBaseActivity<LoginPresenter> implements LoginContract.View {
 
-    private RelativeLayout rlTitleBar;
-    private TextView tvNewUser;
+    @BindView(R.id.tv_new_user)
+    TextView mTvNewUser;
+    @BindView(R.id.rlTitlebar)
+    RelativeLayout mRlTitlebar;
+    @BindView(R.id.iv_login_logo)
+    ImageView mIvLoginLogo;
+    @BindView(R.id.view)
+    View mView;
+    @BindView(R.id.iv_login_userLogo)
+    ImageView mIvLoginUserLogo;
+    @BindView(R.id.et_login_userName)
+    EditTextWithDeleteIcon mEtLoginUserName;
+    @BindView(R.id.user_line)
+    View mUserLine;
+    @BindView(R.id.rl_liner1)
+    RelativeLayout mRlLiner1;
+    @BindView(R.id.iv_login_pswLogo)
+    ImageView mIvLoginPswLogo;
+    @BindView(R.id.et_login_passWord)
+    EditTextWithDeleteIcon mEtLoginPassWord;
+    @BindView(R.id.psw_line)
+    View mPswLine;
+    @BindView(R.id.rl_liner2)
+    RelativeLayout mRlLiner2;
+    @BindView(R.id.btn_login)
+    Button mBtnLogin;
+    @BindView(R.id.ll_translate)
+    LinearLayout mLlTranslate;
+    @BindView(R.id.ll_name_psw)
+    LinearLayout mLlNamePsw;
+    @BindView(R.id.rb_release)
+    RadioButton mRbRelease;
+    @BindView(R.id.rb_test)
+    RadioButton mRbTest;
+    @BindView(R.id.rg_group)
+    RadioGroup mRgGroup;
+    @BindView(R.id.tv_login_desc)
+    TextView mTvLoginDesc;
+    @BindView(R.id.tv_login_register)
+    TextView mTvLoginRegister;
+    @BindView(R.id.background)
+    RelativeLayout mBackground;
 
-    private LinearLayout llNewUser,llTranslate;
-    private ImageView ivLoginLogo;
-    private View mView;
-    private RelativeLayout rlLiner1,rlLiner2;
-    private ImageView ivLoginUserLogo,iv_login_pswLogo;
-    private EditTextWithDeleteIcon etLoginUserName,etLoginPassWord;
-    private View userLine,pswLine;
-    private Button btnLogin;
-    private RadioGroup rgGroup;
-    private RadioButton rbRelease,rbTest;
-    private TextView tvRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
         initViews();
     }
 
     private void initViews() {
-        rlTitleBar = findViewById(R.id.rlTitlebar);
-        tvNewUser = findViewById(R.id.tv_new_user);
-        llNewUser = findViewById(R.id.ll_name_psw);
-        llTranslate = findViewById(R.id.ll_translate);
-        ivLoginLogo = findViewById(R.id.iv_login_logo);
-        mView = findViewById(R.id.view);
-        rlLiner1 = findViewById(R.id.rl_liner1);
-        rlLiner1 = findViewById(R.id.rl_liner2);
-        ivLoginUserLogo = findViewById(R.id.iv_login_userLogo);
-        etLoginUserName = findViewById(R.id.et_login_userName);
-        userLine = findViewById(R.id.user_line);
-        iv_login_pswLogo = findViewById(R.id.iv_login_pswLogo);
-        etLoginPassWord = findViewById(R.id.et_login_passWord);
-        pswLine = findViewById(R.id.psw_line);
-        btnLogin = findViewById(R.id.btn_login);
-        rgGroup = findViewById(R.id.rg_group);
-        rbRelease = findViewById(R.id.rb_release);
-        rbTest = findViewById(R.id.rb_test);
-        tvRegister = findViewById(R.id.tv_login_register);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (null!= mPresenter){
+        if (null != mPresenter) {
             mPresenter.requestLoginData();
         }
     }
@@ -88,6 +103,22 @@ public class LoginActivity extends MVPBaseActivity<LoginPresenter> implements Lo
     @Override
     public void setSomethingData(String msg) {
 //        tv.setText(msg);
+    }
+
+    @OnClick({R.id.btn_login, R.id.tv_login_register})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.et_login_userName:
+            case R.id.et_login_passWord:
+
+                break;
+            case R.id.btn_login:
+                break;
+            case R.id.tv_login_register:
+                break;
+            default:
+                break;
+        }
     }
 }
 
